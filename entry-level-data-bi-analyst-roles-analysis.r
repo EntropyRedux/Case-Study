bf556328ce6c8b82dfd5a1eee7e3f4073b6aae1e
Marvin Bulahan <mbulahan.21@gmail.com>
2024-08-06 11:11:06 +0800

    Kaggle Notebook | Entry-Level Data/BI Analyst Roles Analysis | Version 1

---
+++ entry-level-data-bi-analyst-roles-analysis.r
@@ -0,0 +1,170 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "984c9aa1",
+   "metadata": {
+    "papermill": {
+     "duration": 0.003593,
+     "end_time": "2024-08-06T03:10:57.617443",
+     "exception": false,
+     "start_time": "2024-08-06T03:10:57.613850",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "# Skills Required for Entry-Level Data Analyst and BI Analyst Roles Analysis\n",
+    "**Author : Marvin Bulahan**"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "c3abab1c",
+   "metadata": {
+    "papermill": {
+     "duration": 0.002484,
+     "end_time": "2024-08-06T03:10:57.622832",
+     "exception": false,
+     "start_time": "2024-08-06T03:10:57.620348",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "# Introduction\n",
+    "- In today’s rapidly evolving job market, understanding the skills needed for Entry-Level Data Analyst and BI Analyst roles is crucial for aspiring and new professionals. This case study analyzes job postings from Indeed, Jobstreet, and Upwork to help create a learning plan tailored to the current market.\n",
+    "- Skills for these roles are constantly changing, especially with AI. There are many tools to learn, each requiring time to master. This study highlights the most in-demand skills and tools, providing guidance on planning an efficient learning path for the most relative skills and tools in the industry.\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "d249d93a",
+   "metadata": {
+    "papermill": {
+     "duration": 0.002334,
+     "end_time": "2024-08-06T03:10:57.627702",
+     "exception": false,
+     "start_time": "2024-08-06T03:10:57.625368",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "## Objective\n",
+    "- **Identify** the most in-demand <u>skills and tools<u/> required for these roles.\n",
+    "- **Compare** skill requirements between <u>freelance and regular</u> job postings.\n",
+    "- **Examine** how job type (e.g., full-time, part-time, freelance) and work setup (e.g., on-site, remote) influence required skills and qualifications.\n",
+    "- **Evaluate** the relationship between <u>job responsibilities and skill</u> needs.\n",
+    "- **Explore** the <u>benefits and qualifications</u> commonly associated with these positions.\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "be3f9d53",
+   "metadata": {
+    "papermill": {
+     "duration": 0.002337,
+     "end_time": "2024-08-06T03:10:57.632555",
+     "exception": false,
+     "start_time": "2024-08-06T03:10:57.630218",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "## Methodology\n",
+    "- **Data Cleaning and Manipulation:** Prepare and standardize data to ensure it is accurate and suitable for analysis.\n",
+    "- **Frequency Analysis:** Determine the most frequently occurring values in the dataset.\n",
+    "- **Comparative Analysis:** Compare values across different categories or groups to identify differences and similarities.\n",
+    "- **Correlation Analysis:** Assess relationships between variables to identify potential connections.\n",
+    "- **Statistical Analysis:** Perform descriptive statistics to summarize data and provide insights.\n",
+    "- **Cross Referencing:** Integrate and verify data across different tables to ensure consistency and completeness.\n",
+    "- **Visualization:** Develop charts and graphs to simplify data interpretation and highlight key findings.\n",
+    "- **Reporting:** Compile a comprehensive report detailing the analysis, findings, and insights.\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "c61d610a",
+   "metadata": {
+    "papermill": {
+     "duration": 0.002245,
+     "end_time": "2024-08-06T03:10:57.637157",
+     "exception": false,
+     "start_time": "2024-08-06T03:10:57.634912",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "## Tools\n",
+    "- **PostgreSQL** - Data Preparation and Analysis\n",
+    "- **Tableau** - For Visualizations\n",
+    "- **Kaggle R Notebook** - for Publishing\n",
+    "- **DBdiagram.io** - for database schema diagram\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "7a3b6be2",
+   "metadata": {
+    "papermill": {
+     "duration": 0.002232,
+     "end_time": "2024-08-06T03:10:57.641719",
+     "exception": false,
+     "start_time": "2024-08-06T03:10:57.639487",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "<img src=\"https://drive.google.com/uc?export=view&id=1SWZS_Ds0SOJLrgE1TJOSnfAgONSSUe8n\" width=\"640\" height=\"480\" />"
+   ]
+  }
+ ],
+ "metadata": {
+  "kaggle": {
+   "accelerator": "none",
+   "dataSources": [
+    {
+     "datasetId": 5501098,
+     "sourceId": 9114056,
+     "sourceType": "datasetVersion"
+    }
+   ],
+   "dockerImageVersionId": 30749,
+   "isGpuEnabled": false,
+   "isInternetEnabled": true,
+   "language": "r",
+   "sourceType": "notebook"
+  },
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.4.0"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.639042,
+   "end_time": "2024-08-06T03:10:57.769832",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2024-08-06T03:10:54.130790",
+   "version": "2.6.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
